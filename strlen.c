ec46c04c5add4defbcee2e704b698ea8d62ce78c
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-06-20 19:46:18 +0530

    Add files via upload

---
+++ strlen.c
@@ -0,0 +1,9 @@
+#include <string.h>
+int main() {
+char s1[] ="Tarun";
+char s2[] = "_Sheoram";
+printf("The Length of s1 is %d\n",strlen(s1));
+printf("The Length of s2 is %d\n",strlen(s2));
+
+return 0;
+} 
